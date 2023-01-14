93da1ee079ac41aa981161fe33ad266fdadd73d0
Morgosh <111243966+Mag1cccc@users.noreply.github.com>
2023-01-14 17:56:47 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Open modal
+let vid = document.getElementById("myVideo");
+function openModal() {
+    document.getElementById("myModal").style.display = "flex";
+}
+
+// Close modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+
